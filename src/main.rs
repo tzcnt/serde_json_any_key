b7e275fd9e88a763666d9ca7357686a0cc79dab7
tzcnt <trailingzerocount@gmail.com>
2022-05-31 16:50:44 -0700

    cleanup indentation

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,23 @@ use serde::Serialize;
 
 #[derive(Serialize, PartialEq, Eq, Hash)]
 pub struct Test {
-    pub a: i32,
-    pub b: i32
+  pub a: i32,
+  pub b: i32
 }
 
 fn main() {
-   let mut map = HashMap::<Test, i32>::new();
-   map.insert(Test {
-       a: 5, b: 7
-   }, 9);
-   
-   let serialized = serde_json::to_string(&map);
-   match serialized {
-       Ok(s) => { println!("{}", s); }
-       Err(e) => { println!("Error: {}", e); }
-   }
+  let mut map = HashMap::<Test, Test>::new();
+  map.insert(Test {a: 3, b: 5}, Test {a: 7, b: 9});
 
-   let serialized = serde_json_any_map::to_string(&map);
-   match serialized {
+  let serialized = serde_json::to_string(&map);
+  match serialized {
     Ok(s) => { println!("{}", s); }
     Err(e) => { println!("Error: {}", e); }
-}
+  }
+
+  let serialized = serde_json_any_map::to_string(&map);
+  match serialized {
+    Ok(s) => { println!("{}", s); }
+    Err(e) => { println!("Error: {}", e); }
+  }
 }
