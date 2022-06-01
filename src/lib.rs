84747a361a7712bf46e9eb449264507d77c60d44
tzcnt <trailingzerocount@gmail.com>
2022-05-31 21:21:26 -0700

    Add doctests

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,42 @@ impl<'a, K, V> Serialize for SerializeMapIterWrapper<'a, K, V> where
   }
 }
 
+/// Serialize an Iterator<(&K, &V)> like that given by HashMap::iter().
+/// serde_json::to_string() will be called on each K element during serialization.
+///
+/// # Examples
+/// ```
+/// use std::collections::HashMap;
+/// use serde::Serialize;
+/// use serde_json::Error;
+/// 
+/// #[derive(Clone, Copy, Serialize, PartialEq, Eq, Hash)]
+/// pub struct Test {
+///   pub a: i32,
+///   pub b: i32
+/// }
+/// 
+/// fn try_main() -> Result<(), Error> {
+/// let mut map = HashMap::<Test, Test>::new();
+/// map.insert(Test {a: 3, b: 5}, Test {a: 7, b: 9});
+/// 
+/// // Naive serde_json cannot serialize this map.
+/// let ser1 = serde_json::to_string(&map);
+/// assert_eq!(ser1.err().unwrap().to_string(), "key must be a string");
+/// 
+/// // Use this crate's utility function - elements are serialized lazily.
+/// // Outputs {"{\"a\":3,\"b\":5}":{"a":7,"b":9}}
+/// let ser2 = serde_json_tuple_iter::map_to_json(&mut map.iter()).unwrap();
+///
+/// // Compare to a winded workaround that copies the map.
+/// // Same output
+/// let string_map: HashMap<String, Test> = map.iter().map(|(k, &v)| (serde_json::to_string(k).unwrap(), v)).collect();
+/// let ser3 = serde_json::to_string(&string_map).unwrap();
+///
+/// assert_eq!(ser2, ser3);
+/// Ok(()) }
+/// try_main().unwrap();
+/// ```
 pub fn map_to_json<'a, K, V>(iter: &'a mut dyn Iterator<Item=(&'a K, &'a V)>) -> Result<String, serde_json::Error> where
 K: Serialize,
 V: Serialize
@@ -62,6 +98,37 @@ impl<'a, K, V> Serialize for SerializeVecIterWrapper<'a, K, V> where
   }
 }
 
+/// Serialize an Iterator<&(K, V)> like that given by Vec::<(K, V)>::iter().
+/// serde_json::to_string() will be called on each K element during serialization.
+///
+/// # Examples
+/// ```
+/// use std::collections::HashMap;
+/// use serde::Serialize;
+/// use serde_json::Error;
+/// 
+/// #[derive(Clone, Copy, Serialize, PartialEq, Eq, Hash)]
+/// pub struct Test {
+///   pub a: i32,
+///   pub b: i32
+/// }
+/// 
+/// fn try_main() -> Result<(), Error> {
+/// let v = vec![(Test {a: 3, b: 5}, Test {a: 7, b: 9})];
+/// 
+/// // Naive serde_json will serialize this as an array, not a map.
+/// // Outputs [[{"a":3,"b":5},{"a":7,"b":9}]]
+/// let ser1 = serde_json::to_string(&v).unwrap();
+/// assert_eq!(ser1, "[[{\"a\":3,\"b\":5},{\"a\":7,\"b\":9}]]");
+/// 
+/// // Use this crate's utility function - elements are serialized lazily.
+/// // Outputs {"{\"a\":3,\"b\":5}":{"a":7,"b":9}}
+/// let ser2 = serde_json_tuple_iter::vec_to_json(&mut v.iter()).unwrap();
+///
+/// assert_eq!(ser2, "{\"{\\\"a\\\":3,\\\"b\\\":5}\":{\"a\":7,\"b\":9}}");
+/// Ok(()) }
+/// try_main().unwrap();
+/// ```
 pub fn vec_to_json<'a, K, V>(iter: &'a mut dyn Iterator<Item=&'a (K, V)>) -> Result<String, serde_json::Error> where
 K: Serialize,
 V: Serialize
