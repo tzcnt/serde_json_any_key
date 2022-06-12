bba89b2eb2ac022ae3248da8824cd4a620fc63d7
tzcnt <trailingzerocount@gmail.com>
2022-06-12 15:00:14 -0700

    combine any_key_map / any_key_vec deserialize

--- src/lib.rs
+++ src/lib.rs
@@ -566,96 +566,50 @@ for<'de> V: Deserialize<'de>
   Ok(vec)
 }
 
-pub(crate) struct MapIter<'de, A, K, V> {
-  pub(crate) access: A,
-  marker: PhantomData<(&'de (), K, V)>,
-}
 
-impl<'de, A, K, V> MapIter<'de, A, K, V> {
-    pub(crate) fn new(access: A) -> Self
-    where
-        A: serde::de::MapAccess<'de>,
-    {
-        Self {
-            access,
-            marker: PhantomData,
-        }
-    }
-}
-
-impl<'de, A, K, V> Iterator for MapIter<'de, A, K, V>
-where
-    A: serde::de::MapAccess<'de>,
-    K: Deserialize<'de>,
-    V: Deserialize<'de>,
-{
-    type Item = Result<(K, V), A::Error>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.access.next_entry().transpose()
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        match self.access.size_hint() {
-            Some(size) => (size, Some(size)),
-            None => (0, None),
-        }
-    }
-}
-
-/// Apply the attribute `#[serde(with = "any_key_map")]` to de/serialize structs with nested maps that contain non-string keys.
-/// ```
-/// use std::collections::HashMap;
-/// use serde::{Serialize, Deserialize};
-/// use serde_json::Error;
-/// use serde_json_any_key::*;
-/// 
-/// #[derive(Clone, Copy, Serialize, Deserialize, PartialEq, Eq, Hash, Debug)]
-/// pub struct Test {
-///   pub a: i32,
-///   pub b: i32
-/// }
-/// 
-/// #[derive(Clone, Default, Serialize, Deserialize, PartialEq, Eq, Debug)]
-/// pub struct WithNestedMap {
-///   #[serde(with = "any_key_map")]
-///   pub struct_map: HashMap<Test, Test>,
-///   #[serde(with = "any_key_map")]
-///   pub int_map: HashMap<i32, String>
-/// }
-/// 
-/// fn try_main() -> Result<(), Error> {
-/// let mut data: WithNestedMap = Default::default();
-/// data.struct_map.insert(Test {a: 3, b: 5}, Test {a: 7, b: 9});
-/// data.int_map.insert(5, "foo".to_string());
-/// 
-/// let ser = serde_json::to_string(&data).unwrap();
-/// let deser: WithNestedMap = serde_json::from_str(&ser).unwrap();
-///
-/// assert_eq!(data, deser);
-/// Ok(()) }
-/// try_main().unwrap();
-/// ```
-pub mod any_key_map {
-use super::*;
-use serde::de::{MapAccess};
-use std::fmt;
-
-  /// See docs for [any_key_map](index.html).
-  pub fn serialize<'s, S, C, K, V>(coll: C, serializer: S) -> Result<S::Ok, S::Error>
-  where S: Serializer,
-  C: IntoIterator<Item=(&'s K,&'s V)>,
-  K: Serialize + Any + 's,
-  V: Serialize + 's
-  {
-    let mut iter = coll.into_iter();
-    let wrap = SerializeMapIterWrapper {
-      iter: RefCell::new(&mut iter),
-    };
-    wrap.serialize(serializer)
+mod serde_with_utils {
+  use super::*;
+  use serde::de::{MapAccess};
+  use std::fmt;
+  pub struct MapIter<'de, A, K, V> {
+    pub access: A,
+    marker: PhantomData<(&'de (), K, V)>,
   }
 
-  /// See docs for [any_key_map](index.html).
+  impl<'de, A, K, V> MapIter<'de, A, K, V> {
+      pub fn new(access: A) -> Self
+      where
+          A: serde::de::MapAccess<'de>,
+      {
+          Self {
+              access,
+              marker: PhantomData,
+          }
+      }
+  }
+
+  impl<'de, A, K, V> Iterator for MapIter<'de, A, K, V>
+  where
+      A: serde::de::MapAccess<'de>,
+      K: Deserialize<'de>,
+      V: Deserialize<'de>,
+  {
+      type Item = Result<(K, V), A::Error>;
+
+      fn next(&mut self) -> Option<Self::Item> {
+          self.access.next_entry().transpose()
+      }
+
+      fn size_hint(&self) -> (usize, Option<usize>) {
+          match self.access.size_hint() {
+              Some(size) => (size, Some(size)),
+              None => (0, None),
+          }
+      }
+  }
+
+  // any_key_map and any_key_vec use the same deserialize function
+  #[inline]
   pub fn deserialize<'d, D, C, K, V>(deserializer: D) -> Result<C, D::Error> where
     D: Deserializer<'d>,
     C: FromIterator<(K,V)> + Sized,
@@ -701,6 +655,68 @@ use std::fmt;
     }
     
     deserializer.deserialize_map(Helper(PhantomData))
+  }
+}
+
+/// Apply the attribute `#[serde(with = "any_key_map")]` to de/serialize structs with nested maps that contain non-string keys.
+/// ```
+/// use std::collections::HashMap;
+/// use serde::{Serialize, Deserialize};
+/// use serde_json::Error;
+/// use serde_json_any_key::*;
+/// 
+/// #[derive(Clone, Copy, Serialize, Deserialize, PartialEq, Eq, Hash, Debug)]
+/// pub struct Test {
+///   pub a: i32,
+///   pub b: i32
+/// }
+/// 
+/// #[derive(Clone, Default, Serialize, Deserialize, PartialEq, Eq, Debug)]
+/// pub struct WithNestedMap {
+///   #[serde(with = "any_key_map")]
+///   pub struct_map: HashMap<Test, Test>,
+///   #[serde(with = "any_key_map")]
+///   pub int_map: HashMap<i32, String>
+/// }
+/// 
+/// fn try_main() -> Result<(), Error> {
+/// let mut data: WithNestedMap = Default::default();
+/// data.struct_map.insert(Test {a: 3, b: 5}, Test {a: 7, b: 9});
+/// data.int_map.insert(5, "foo".to_string());
+/// 
+/// let ser = serde_json::to_string(&data).unwrap();
+/// let deser: WithNestedMap = serde_json::from_str(&ser).unwrap();
+///
+/// assert_eq!(data, deser);
+/// Ok(()) }
+/// try_main().unwrap();
+/// ```
+pub mod any_key_map {
+use super::*;
+
+  /// See docs for [any_key_map](index.html).
+  pub fn serialize<'s, S, C, K, V>(coll: C, serializer: S) -> Result<S::Ok, S::Error>
+  where S: Serializer,
+  C: IntoIterator<Item=(&'s K,&'s V)>,
+  K: Serialize + Any + 's,
+  V: Serialize + 's
+  {
+    let mut iter = coll.into_iter();
+    let wrap = SerializeMapIterWrapper {
+      iter: RefCell::new(&mut iter),
+    };
+    wrap.serialize(serializer)
+  }
+
+  /// See docs for [any_key_map](index.html).
+  pub fn deserialize<'d, D, C, K, V>(deserializer: D) -> Result<C, D::Error> where
+    D: Deserializer<'d>,
+    C: FromIterator<(K,V)> + Sized,
+    for<'de> K: Deserialize<'de> + Any + 'd,
+    for<'de> V: Deserialize<'de> + 'd,
+  {
+    // any_key_map and any_key_vec use the same deserialize function
+    serde_with_utils::deserialize::<'d,D,C,K,V>(deserializer)
   }
 }
 
@@ -760,8 +776,6 @@ use std::fmt;
 /// ```
 pub mod any_key_vec {
   use super::*;
-  use serde::de::{MapAccess};
-  use std::fmt;
 
   /// See docs for [any_key_vec](index.html).
   pub fn serialize<'s, S, C, K, V>(coll: C, serializer: S) -> Result<S::Ok, S::Error>
@@ -784,45 +798,8 @@ pub mod any_key_vec {
     for<'de> K: Deserialize<'de> + Any + 'd,
     for<'de> V: Deserialize<'de> + 'd,
   {
-    struct Helper<C,K,V>(PhantomData<(C,K,V)>);
-    impl<'d,C,K,V> Visitor<'d> for Helper<C,K,V>
-    where
-    C: FromIterator<(K,V)> + Sized,
-    for<'de> K: Deserialize<'de> + Any + 'd,
-    for<'de> V: Deserialize<'de> + 'd
-    {
-        type Value = C;
-
-        fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-            write!(formatter, "a JSON map")
-        }
-
-        fn visit_map<A>(self, seq: A) -> Result<Self::Value, A::Error>
-        where
-            A: MapAccess<'d>,
-        {
-          // https://stackoverflow.com/a/26370894/19260728
-          let coll: Result<C, A::Error> = MapIter::<'d, A, String, V>::new(seq)
-            .map(|res| {
-              res.and_then(|value: (String,V)| {
-                let key_obj: K = match TypeId::of::<K>() == TypeId::of::<String>() {
-                  true => match <K as Deserialize>::deserialize(serde_json::Value::from(value.0)) {
-                    Ok(k) => k,
-                    Err(e) => { return Err(e).map_err(serde::de::Error::custom); }
-                  },
-                  false => match serde_json::from_str(&value.0) {
-                    Ok(k) => k,
-                    Err(e) => { return Err(e).map_err(serde::de::Error::custom); }
-                  }
-                };
-                Ok((key_obj, value.1))
-              })
-            }).collect();
-          coll
-        }
-    }
-    
-    deserializer.deserialize_map(Helper(PhantomData))
+    // any_key_map and any_key_vec use the same deserialize function
+    serde_with_utils::deserialize::<'d,D,C,K,V>(deserializer)
   }
 }
 
